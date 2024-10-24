68c899e2d28d043fa97501c64820370648f8c309
yoriscape <yoriscape.git@gmail.com>
2024-10-24 17:15:53 +0000

    peridot: lineage--->Neoteric
    Signed-off-by: yoriscape <yoriscape.git@gmail.com>

---
+++ peridot.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the Neoteric configuration.
+$(call inherit-product, vendor/neoteric/target/product/neoteric-target.mk)
+
+# Inherit from peridot device
+$(call inherit-product, device/xiaomi/peridot/device.mk)
+
+PRODUCT_NAME := peridot
+PRODUCT_DEVICE := peridot
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 24069PC21G
+
+PRODUCT_SYSTEM_NAME := peridot_global
+PRODUCT_SYSTEM_DEVICE := peridot
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="peridot_global-user 14 UKQ1.240116.001 V816.0.5.0.UNPMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.5.0.UNPMIXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
